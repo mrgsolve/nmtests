2e14e32ef25b8bb85edb5fb80d249e7a7925a777
Kyle Baron <kyleb@metrumrg.com>
2017-11-08 15:40:50 +0000

    re-run nmtest-2 with 0.8.10

--- install.R
+++ install.R
@@ -1,9 +1,9 @@
-.libPaths("~/Rlibs/")
-
+.libPaths("/data/Rlibs/")
 
 library(devtools)
 
-devtools::install_github("metrumresearchgroup/mrgsolve")
+devtools::install_github("metrumresearchgroup/mrgsolve", ref = "v0.8.10")
+
 
 devtools::install_github("metrumresearchgroup/metrumrg")
 
